fc81cca8a67ddc0e214d8fae11b7afc77172cef8
kir <kondrashovks1989@gmail.com>
2017-02-28 06:47:09 +0100

    Minor changes

--- native/common.mk
+++ native/common.mk
@@ -1,11 +1,11 @@
 -include $(DEPTH)/../build_spec/native/specific.mk
 
 # Каталог компиляции программ
-BIN = $(INS)/bin
+BIN ?= $(INS)/bin
 # Каталог с настроечными файлами
-ETC = $(INS)/etc
-LIBS = $(INS)/lib
-USRINC = $(INS)/usr/include
+ETC ?= $(INS)/etc
+LIBS ?= $(INS)/lib
+USRINC ?= $(INS)/usr/include
 # Операционная система (linux/qnx)
 SYS ?= $(shell uname -s | tr A-Z a-z)
 
