838f91b960703b7836ebb7f41fda8e81cf96bea9
kir <kondrashovks1989@gmail.com>
2017-02-28 05:55:37 +0100

    Native build modified

--- native/common.mk
+++ native/common.mk
@@ -1,17 +1,16 @@
 -include $(DEPTH)/../build_spec/native/specific.mk
 
 # Каталог компиляции программ
-BIN ?= $(INS)/bin
+BIN = $(INS)/bin
 # Каталог с настроечными файлами
-ETC ?= $(INS)/etc
-LIBS ?= $(INS)/lib
-USRINC ?= $(DEPTH)
+ETC = $(INS)/etc
+LIBS = $(INS)/lib
+USRINC = $(INS)/usr/include
 # Операционная система (linux/qnx)
 SYS ?= $(shell uname -s | tr A-Z a-z)
 
 CXX = g++ -g
-CXXFLAGS =-I$(USRINC) -I$(DEPTH)/../../install/usr/include
+CXXFLAGS =-I$(USRINC)
 AR = ar
 OSUF = o
 OBJDIR = obj_nat
-NO_HEADERINS = 1
